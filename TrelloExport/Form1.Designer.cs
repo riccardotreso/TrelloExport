a7e62c0f48add1e1d3b0268dfe586b8f481051c4
riccardotreso <riccardo.treso@hotmail.com>
2014-09-09 11:10:48 +0200

    First Commit

---
+++ TrelloExport/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace TrelloExport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnGetToken = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.txtBoardID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(347, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AppKey";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(288, 114);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(65, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Go";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(372, 169);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnExport);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(364, 143);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "AppKey";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnGetToken);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.txtToken);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(364, 143);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Token";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnGetToken
+            // 
+            this.btnGetToken.Location = new System.Drawing.Point(126, 114);
+            this.btnGetToken.Name = "btnGetToken";
+            this.btnGetToken.Size = new System.Drawing.Size(99, 23);
+            this.btnGetToken.TabIndex = 8;
+            this.btnGetToken.Text = "Get New Token";
+            this.btnGetToken.UseVisualStyleBackColor = true;
+            this.btnGetToken.Click += new System.EventHandler(this.btnGetToken_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(288, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(6, 26);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(347, 20);
+            this.txtToken.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Token";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.txtBoardID);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.button5);
+            this.tabPage3.Controls.Add(this.button4);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(364, 143);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Board";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // txtBoardID
+            // 
+            this.txtBoardID.Location = new System.Drawing.Point(6, 29);
+            this.txtBoardID.Name = "txtBoardID";
+            this.txtBoardID.Size = new System.Drawing.Size(347, 20);
+            this.txtBoardID.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "BoardID";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(288, 114);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Go";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(9, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "https://trello.com/1/appKey/generate";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 193);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox txtBoardID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnGetToken;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TrelloExport/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using TrelloNet;
+
+
+namespace TrelloExport
+{
+    public partial class Form1 : Form
+    {
+
+        String appKey;
+        String token;
+        ITrello trello;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // read user value
+            if (File.Exists("data.txt"))
+            {
+                var s = File.ReadAllLines("data.txt");
+                if (s.Length > 0)
+                {
+                    if (!string.IsNullOrEmpty(s[0]))
+                        appKey = this.textBox1.Text = s[0];
+
+
+                    if (s.Length > 1 && !string.IsNullOrEmpty(s[1]))
+                        token = this.txtToken.Text = s[1];
+                }
+            }
+
+
+        }
+
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(appKey) || appKey != this.textBox1.Text)
+            {
+                string[] tow = new string[] { this.textBox1.Text, token };
+                File.WriteAllLines("data.txt", tow);
+            }
+
+            appKey = this.textBox1.Text;
+            trello = new Trello(appKey);
+
+            this.tabControl1.SelectedIndex = 1;
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(token) || token != this.txtToken.Text)
+            {
+                string[] tow = new string[] { appKey, this.txtToken.Text };
+                File.WriteAllLines("data.txt", tow);
+            }
+
+            token = this.txtToken.Text;
+            trello.Authorize(token);
+
+
+            this.tabControl1.SelectedIndex = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.tabControl1.SelectedIndex = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.tabControl1.SelectedIndex = 1;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //DO Export
+            Board BoardToExport;
+            IEnumerable<Card> cards;
+            try
+            {
+                BoardToExport = trello.Boards.WithId(this.txtBoardID.Text);
+                cards = trello.Cards.ForBoard(BoardToExport, BoardCardFilter.Visible);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            // write excel here
+            CreateExcelDoc excell_app = new CreateExcelDoc();
+            //creates the main header
+            excell_app.createHeaders(1, 1, "List name");
+            excell_app.createHeaders(1, 2, "Name");
+            excell_app.createHeaders(1, 3, "Description");
+            excell_app.createHeaders(1, 4, "Closed");
+            excell_app.createHeaders(1, 5, "Due Date");
+            excell_app.createHeaders(1, 6, "URL");
+            int i = 2;
+            foreach (var c in cards) {
+                var list = trello.Lists.WithId(c.IdList);
+                excell_app.addData(i, 1, list.Name);
+                excell_app.addData(i, 2, c.Name);
+                excell_app.addData(i, 3, c.Desc);
+                excell_app.addData(i, 4, c.Closed.ToString());
+                excell_app.addData(i, 5, c.Due.HasValue ? c.Due.Value.ToString("dd/MM/yyyy"): string.Empty);
+                excell_app.addData(i, 6, c.Url);
+                ++i;
+
+            }
+            //excell_app.close();
+
+        }
+
+        private void btnGetToken_Click(object sender, EventArgs e)
+        {
+            
+            var url = trello.GetAuthorizationUrl("Trello Export", Scope.ReadWrite);
+
+            // open a web page browser
+            ProcessStartInfo sInfo = new ProcessStartInfo(url.ToString());
+            Process.Start(sInfo);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo(label4.Text);
+            Process.Start(sInfo);
+        }
+
+
+    }
+
+
+    class CreateExcelDoc
+    {
+        private Excel.Application app = null;
+        private Excel.Workbook workbook = null;
+        private Excel.Worksheet worksheet = null;
+        private Excel.Range workSheet_range = null;
+
+        public CreateExcelDoc()
+        {
+            createDoc();
+        }
+
+        public void createDoc()
+        {
+            try
+            {
+                app = new Excel.Application();
+                app.Visible = true;
+                workbook = app.Workbooks.Add(1);
+                worksheet = (Excel.Worksheet)workbook.Sheets[1];
+            }
+            catch (Exception e)
+            {
+                Console.Write("Error");
+            }
+            finally
+            {
+            }
+        }
+
+        public void close()
+        {
+            workbook.Save();
+            workbook.Close();
+        }
+
+        public void createHeaders(int row, int col, string htext)
+        {
+            worksheet.Cells[row, col] = htext;
+        }
+
+        public void addData(int row, int col, string data)
+        {
+            worksheet.Cells[row, col] = data;
+        }
+    }
+}
